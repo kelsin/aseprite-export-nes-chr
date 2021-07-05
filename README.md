a5cd3a123c6ce24aa386850c185ae2048f6429ed
Christopher Giroir <kelsin@valefor.com>
2021-07-05 16:46:59 -0700

    feat: added binary output support

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Current limitations:
 * Sprite must be 128x128 pixels
 * Sprite must be using indexed colors
 * The data you want to export must be on frame 1
-* Only `nesasm` format supported
+* Only pure `binary` and `nesasm` format supported
 
 I'm open to changing all of these limitations as the need arises. Right now this
 serves my purposes!
@@ -32,21 +32,41 @@ You can assign the "Export NES CHR" command a shortcut if you wish.
 Now load up a 128x128 indexed sprite and run the "Export NES CHR" command. If
 your sprite isn't valid for export you will get a message saying so. You are
 then presented with a Dialog that asks you for the output filename and format.
-We default the filename to the sprite's filename with `inc` added (for
-"include"). Click OK and the exported file will be created. Then just include in
-your game!
+We default the filename to the sprite's filename with `chr` added. Click OK and
+the exported file will be created. Then just include in your game!
+
+The plugin will use your color's `index % 4` to get the color to use in this
+macro. I suggest you use any color palette that is a multiple of 4 colors. I
+included a simple `chr.pal` in this repo that is a good example one. You can
+also make a palette based on your real NES palette with all 16 colors (keep in
+mind that the first in each group is either the background color or
+transparant). Please keep in mind that this plugin just saves an index of 0-3
+since that's all that the NES CHR memory requires. Selecting a palette on the
+NES hardware for each sprite or group of background tiles is done in different
+ways.
 
 ## Formats
 
-Right now the only supported format is for
-[nesasm](https://github.com/camsaul/nesasm)'s `.defchr` macro. If other formats
-are needed please let me know!
+Right now we only supprt `binary` and `nesasm` format.
+
+### binary
+
+Binary output creates a binary file which contains the exact bytes needed for a
+page of chr memory in a NES rom. As an example this can be included using
+[`ca65`](https://cc65.github.io/)'s `.incbin` directive:
+
+``` assembly
+.segment "TILES"
+.incbin "background.chr"
+```
 
 ### nesasm
 
-This uses the nesasm `.defchr` macro where each sprite is defined as one macro:
+This format results in a text file that can be included in your
+[`nesasm`](https://github.com/camsaul/nesasm) assembled program. It makes use of
+the `.defchr` macro that `nesasm` provides:
 
-```
+```assembly
 ;;; Sprite 0x0
     .defchr $00033000,\
             $00333300,\
@@ -57,10 +77,3 @@ This uses the nesasm `.defchr` macro where each sprite is defined as one macro:
             $03300330,\
             $00000000
 ```
-
-The plugin will use your color's `index % 4` to get the color to use in this
-macro. I suggest you use any color palette that is a multiple of 4 colors. I
-included a simple `chr.pal` in this repo that is a good example one. You can
-also make a palette based on your real NES palette with all 16 colors (keep in
-mind that the first in each group is either the background color or transparant)
-and use that since we will only place 0-3 in the final output.
--- export-nes-chr.lua
+++ export-nes-chr.lua
@@ -1,5 +1,7 @@
+-- Output the sprite at position sprite_x and sprite_y in nesasm .defchr format
+function writeSpriteNesasm(img, sprite_x, sprite_y)
+    io.write(string.format(";;; Sprite %dx%d\n", sprite_x, sprite_y))
 
-function writeSprite(img, sprite_x, sprite_y)
     for row = 0, 7 do
         local first_row = row == 0
         local last_row = row == 7
@@ -29,20 +31,72 @@ function writeSprite(img, sprite_x, sprite_y)
     end
 end
 
+-- Output the sprite at position sprite_x and sprite_y in binary format
+function writeSpriteBinary(img, sprite_x, sprite_y)
+    -- Low byte
+    for row = 0, 7 do
+        local pixel_y = (sprite_y * 8) + row
+        local byte = 0
+
+        for col = 0, 7 do
+            local pixel_x = (sprite_x * 8) + col
+            local index = img:getPixel(pixel_x, pixel_y)
+            local chr_index = index % 4
+
+            byte = byte * 2
+            if chr_index == 1 or chr_index == 3 then
+                byte = byte + 1
+            end
+        end
+
+        -- Write the byte to the file
+        io.write(string.char(byte))
+    end
+
+    -- High byte
+    for row = 0, 7 do
+        local pixel_y = (sprite_y * 8) + row
+        local byte = 0
+
+        for col = 0, 7 do
+            local pixel_x = (sprite_x * 8) + col
+            local index = img:getPixel(pixel_x, pixel_y)
+            local chr_index = index % 4
+
+            byte = byte * 2
+            if chr_index == 2 or chr_index == 3 then
+                byte = byte + 1
+            end
+        end
+
+        -- Write the byte to the file
+        io.write(string.char(byte))
+    end
+end
+
 -- Output the CHR data from the current file to the passed in filename
-function exportChrData(sprite, filename)
+function exportChrData(sprite, filename, format)
     -- Make a image out of the full sprite
     local img = Image(sprite.spec)
     img:drawSprite(sprite, 1)
 
-    local f = io.open(filename, "w")
+    local options = "w"
+    if format == "binary" then
+        options = options .. "b"
+    end
+
+    -- Open the file for writing and set out default output to the file
+    local f = io.open(filename, options)
     io.output(f)
 
     for sprite_y = 0, 15 do
         for sprite_x = 0, 15 do
-            -- Output a helpful comment
-            io.write(string.format(";;; Sprite %dx%d\n", sprite_x, sprite_y))
-            writeSprite(img, sprite_x, sprite_y)
+            if format == "binary" then
+                writeSpriteBinary(img, sprite_x, sprite_y)
+            else
+                writeSpriteNesasm(img, sprite_x, sprite_y)
+            end
+
         end
     end
     io.close(f)
@@ -75,7 +129,7 @@ function exportNesChr(plugin)
     if filename == "" then
         filename = "chr.inc"
     else
-        filename = string.gsub(filename, "%.[%w]+$", ".inc")
+        filename = string.gsub(filename, "%.[%w]+$", ".chr")
     end
 
     -- Show a dialog to get the output file and then export current sprites data
@@ -86,18 +140,18 @@ function exportNesChr(plugin)
               save=true,
               entry=true,
               filename=filename,
-              filetypes={"asm","inc"}}
+              filetypes={ "chr", "asm", "inc" }}
     dlg:combobox{ id="format",
                   label="Format",
-                  option="nesasm",
-                  options={ "nesasm" }}
+                  option=plugin.preferences.format,
+                  options={ "binary", "nesasm" }}
     dlg:button{ id="ok", text="OK" }
     dlg:button{ id="cancel", text="Cancel" }
     dlg:show()
     local data = dlg.data
     if data.ok then
         plugin.preferences.format = data.format
-        exportChrData(sprite, data.exportFile)
+        exportChrData(sprite, data.exportFile, data.format)
     end
 end
 
@@ -105,7 +159,7 @@ end
 function init(plugin)
     -- Setup initial format preference if not set
     if plugin.preferences.format == nil then
-        plugin.preferences.format = "nesasm"
+        plugin.preferences.format = "binary"
     end
 
     plugin:newCommand{
